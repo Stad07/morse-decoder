c427936c37363c09d1cf8f3637a51db0f6cc91eb
Stanislav <stad-777@yandex.ru>
2020-03-02 19:46:27 +0300

    feat: finish morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,58 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let splitExp = expr.split('  ');	
+	let word = [];
+	let morse = [];
+	let decodeString = '';
+			
+	let round = splitExp[0].length / 10;
+	let current = 0;
+	let next = current + 10;
+	let i = 1;
+	
+	while(i <= round) {
+		let letter = splitExp[0].slice(current, next);		
+		word.push(letter);
+		current = next;
+		next += 10;			
+		i++;
+	}	
+	
+	for(let k = 0; k < word.length; k++) {
+		let str = '';
+		let pos = 0;
+			 
+		if( word[k].includes('*') ) {
+			morse.push(' ');
+		} else {
+
+			while(true) {
+				let foundPos = word[k].indexOf('1', pos);				
+				if(foundPos == -1) break;
+
+				if(word[k].charAt(foundPos+1) == 0) {
+					str += '.';				
+			    } else {
+					 str += '-';	
+				}
+
+				pos = foundPos + 2; 
+			}
+			
+			morse.push(str);
+		}					
+	}
+			
+	for(let j of morse) {
+		if(j == ' ') {
+			decodeString += ' ';
+		} else {
+			decodeString += MORSE_TABLE[j];
+		}
+	}
+					
+	return decodeString;
 }
 
 module.exports = {
